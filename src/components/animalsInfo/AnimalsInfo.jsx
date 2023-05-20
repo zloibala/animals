8d5686fc83c0c9402ea735b5f99ce7e443042082
zloibala <boronova25@gmail.com>
2023-05-20 11:50:37 -0500

    added components

--- src/components/animalsInfo/AnimalsInfo.jsx
+++ src/components/animalsInfo/AnimalsInfo.jsx
@@ -1,24 +1,69 @@
+import React, { useState } from "react";
+import lionImg from "../../assets/images/lion.png";
 import "./animalsInfo.style.css";
 
-const AnimalsInfo = () => {
-  const zebraOnClick = () => {};
+const animalImages = [
+  {
+    name: "zebra",
+    src: "https://www.meme-arsenal.com/memes/f58461a7a10158453d63d047b10dbbe2.jpg",
+    alt: " Zebra is black and white",
+    text: " Zebra is black and white lika a panda",
+  },
+  {
+    name: "lion",
+    src: lionImg,
+    alt: "Lion is the kink of Safari",
+    text: "I am king of safari",
+  },
+];
 
-  return (
-    <div className="animals">
-      <h3>Zebra</h3>
+const AnimalsInfo = () => {
+  const [currentImageInfo, setCurrentImageInfo] = useState("");
+  const imageOnClick = (e) => {
+    const { name, accessKey } = e.target;
+    console.log(name, ":", accessKey);
+    setCurrentImageInfo(animalImages[accessKey].text);
+  };
+
+  const showAllImages = animalImages.map((animal, index) => {
+    return (
       <img
-        src="https://www.meme-arsenal.com/memes/f58461a7a10158453d63d047b10dbbe2.jpg"
-        onClick={zebraOnClick}
+        accessKey={index}
+        src={animal.src}
+        name={animal.name}
+        key={index}
+        alt={animal.alt}
+        onClick={imageOnClick}
       />
-      <h3> Lion </h3>
-      <img src="https://cdn.shopify.com/s/files/1/0279/0234/5304/products/1900-01918-002_Madagascar_Flexing_Alex_RealBig_002_PROD.jpg?v=1620681690&width=1946" />
-      <h3> Hippo </h3>
-      <img src="https://vignette.wikia.nocookie.net/jadensadventures/images/b/b7/Madagascar-dreamworks-gloria4g.jpg/revision/latest?cb=20120908220950" />
-      <h3> Giraffe </h3>
-      <img src="https://i.pinimg.com/originals/3b/4b/18/3b4b18dd3b8e74de41286133ba7fa122.png" />
-      <h3> Pinguin </h3>
-      <img src="https://en.wikifur.com/w/images/thumb/3/3a/Madagasca_Penguins.jpg/800px-Madagasca_Penguins.jpg" />
-    </div>
+    );
+  });
+
+  //   const showInfo= () => (
+
+  //   );
+  return (
+    <>
+      <h1> Animals World</h1>
+      {showAllImages}
+      {currentImageInfo}
+
+      {/* <div className="animals">
+        <img
+          src="https://www.meme-arsenal.com/memes/f58461a7a10158453d63d047b10dbbe2.jpg"
+          onClick={imageOnClick}
+        />
+        {showText && <p> Zebra baby</p>}
+        <img src={lionImg} onClick={imageOnClick} />
+        {showText && <p> Lion baby</p>}
+        <img
+          src="https://vignette.wikia.nocookie.net/jadensadventures/images/b/b7/Madagascar-dreamworks-gloria4g.jpg/revision/latest?cb=20120908220950"
+          onClick={imageOnClick}
+        />
+        {showText && <p> Hippo mama</p>}
+        <img src="https://i.pinimg.com/originals/3b/4b/18/3b4b18dd3b8e74de41286133ba7fa122.png" />
+        <img src="https://en.wikifur.com/w/images/thumb/3/3a/Madagasca_Penguins.jpg/800px-Madagasca_Penguins.jpg" />
+      </div> */}
+    </>
   );
 };
 
